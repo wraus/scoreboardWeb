6c0a5efa17330dca297e08414ddb687cf5614054
David Howe <davidrh2202@gmail.com>
2015-08-31 20:46:21 +1000

    Updates to support war deployment
    Needed hsqldb

--- src/main/java/au/org/howe/scoreboard/Application.java
+++ src/main/java/au/org/howe/scoreboard/Application.java
@@ -6,10 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@PropertySource("classpath:application.properties")
 public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
