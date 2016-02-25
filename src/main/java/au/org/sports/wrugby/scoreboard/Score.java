a3121a1d07654a39a8bf0c878975a6ba2cd0f899
Hay <richard.hay@auspost.com.au>
2016-02-25 12:29:02 +1100

    updated with accepted model for clock, team and score

--- src/main/java/au/org/sports/wrugby/scoreboard/Score.java
+++ src/main/java/au/org/sports/wrugby/scoreboard/Score.java
@@ -2,96 +2,47 @@ package au.org.sports.wrugby.scoreboard;
 
 public class Score {
 
-    private Integer team1Score = 0;
-    private Integer team2Score = 0;
-
-    private String team1Name;
-    private String team2Name;
-
-    private Integer gameClockMins = 0;
-    private Integer gameClockSecs = 0;
-    private Integer gameClockTenthSecs = 0;
-
-    private Integer shotClockSecs = 0;
-    private Integer shotClockTenthSecs = 0;
-
+    private Team team1;
+    private Team team2;
+    private Clock gameClock;
+    private Clock shotClock;
     private Integer period = 1;
-    private String possession;
+    private String direction;
 
     public Score() {
 
     }
 
-    public Integer getTeam1Score() {
-        return team1Score;
+    public Team getTeam1() {
+        return team1;
     }
 
-    public void setTeam1Score(Integer team1Score) {
-        this.team1Score = team1Score;
+    public void setTeam1(Team team1) {
+        this.team1 = team1;
     }
 
-    public Integer getTeam2Score() {
-        return team2Score;
+    public Team getTeam2() {
+        return team2;
     }
 
-    public void setTeam2Score(Integer team2Score) {
-        this.team2Score = team2Score;
+    public void setTeam2(Team team2) {
+        this.team2 = team2;
     }
 
-    public String getTeam1Name() {
-        return team1Name;
+    public Clock getGameClock() {
+        return gameClock;
     }
 
-    public void setTeam1Name(String team1Name) {
-        this.team1Name = team1Name;
+    public void setGameClock(Clock gameClock) {
+        this.gameClock = gameClock;
     }
 
-    public String getTeam2Name() {
-        return team2Name;
+    public Clock getShotClock() {
+        return shotClock;
     }
 
-    public void setTeam2Name(String team2Name) {
-        this.team2Name = team2Name;
-    }
-
-    public Integer getGameClockMins() {
-        return gameClockMins;
-    }
-
-    public void setGameClockMins(Integer gameClockMins) {
-        this.gameClockMins = gameClockMins;
-    }
-
-    public Integer getGameClockSecs() {
-        return gameClockSecs;
-    }
-
-    public void setGameClockSecs(Integer gameClockSecs) {
-        this.gameClockSecs = gameClockSecs;
-    }
-
-    public Integer getGameClockTenthSecs() {
-        return gameClockTenthSecs;
-    }
-
-    public void setGameClockTenthSecs(Integer gameClockTenthSecs) {
-        this.gameClockTenthSecs = gameClockTenthSecs;
-    }
-
-    public Integer getShotClockSecs() {
-        return shotClockSecs;
-    }
-
-    public void setShotClockSecs(Integer shotClockSecs) {
-        this.shotClockSecs = shotClockSecs;
-    }
-
-    public Integer getShotClockTenthSecs() {
-        return shotClockTenthSecs;
-    }
-
-    public void setShotClockTenthSecs(Integer shotClockTenthSecs) {
-        this.shotClockTenthSecs = shotClockTenthSecs;
+    public void setShotClock(Clock shotClock) {
+        this.shotClock = shotClock;
     }
 
     public Integer getPeriod() {
@@ -102,12 +53,11 @@ public class Score {
         this.period = period;
     }
 
-    public String getPossession() {
-        return possession;
+    public String getDirection() {
+        return direction;
     }
 
-    public void setPossession(String possession) {
-        this.possession = possession;
+    public void setDirection(String direction) {
+        this.direction = direction;
     }
-
 }
