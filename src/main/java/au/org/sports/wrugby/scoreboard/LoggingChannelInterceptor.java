a3b5edea8af8e74a026324cda9209f0bb7a780b9
David Howe <david.howe@auspost.com.au>
2016-02-26 03:05:16 +1100

    US24 I want all scoreboard changes to be logged to a file

---
+++ src/main/java/au/org/sports/wrugby/scoreboard/LoggingChannelInterceptor.java
@@ -0,0 +1,53 @@
+package au.org.sports.wrugby.scoreboard;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.xml.XmlMapper;
+import org.apache.commons.io.FileUtils;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptorAdapter;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.springframework.messaging.simp.SimpMessageType.*;
+
+@Component("loggingChannelInterceptor")
+public class LoggingChannelInterceptor extends ChannelInterceptorAdapter implements InitializingBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingChannelInterceptor.class);
+
+    @Value("${au.org.sports.wrugby.scoreboard.logFile}")
+    public String fileName;
+
+    private File file;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        file = new File(fileName);
+    }
+
+    @Override
+    public void afterSendCompletion(Message<?> message, MessageChannel channel, boolean sent, Exception ex) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        if (accessor.getCommand().getMessageType() == MESSAGE) {
+            String scoreJSON = new String ((byte[]) message.getPayload());
+            try {
+                ObjectMapper jsonMapper = new ObjectMapper();
+                Scoreboard score = jsonMapper.readValue(scoreJSON, Scoreboard.class);
+                XmlMapper xmlMapper = new XmlMapper();
+                xmlMapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
+                FileUtils.writeStringToFile(file, xmlMapper.writeValueAsString(score) + "\n", true);
+            } catch (IOException e) {
+                // As this is an interceptor, we don't want the exception to propogate
+                LOGGER.error("Unable to write score to file", e);
+            }
+        }
+    }
+}
